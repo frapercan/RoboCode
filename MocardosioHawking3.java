6156f43321de9f88ca14007ec174c19a5b4b88b7
frapercan <frapercan1@gmail.com>
2017-02-22 16:59:47 +0100

    Add files via upload
    Last modification. 
    -Offset adjusted
    -Victory dance

--- MocardosioHawking3.java
+++ MocardosioHawking3.java
@@ -5,21 +5,19 @@ import robocode.*;
 import robocode.util.Utils;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
 
 public class MocardosioHawking3 extends AdvancedRobot {
 	double Mov = 1; //Variable que indica si vamos hacia adelante o hacia atras
 	double pi = Math.PI;
-	double randomness = Math.random();
 	public void run() {
 		
-		setBodyColor(Color.BLUE);
-		setGunColor(Color.BLUE);
-		setRadarColor(Color.BLUE);
-		setBulletColor(Color.BLUE);
+		setBodyColor(Color.ORANGE);
+		setGunColor(Color.PINK);
+		setRadarColor(Color.RED);
+		setBulletColor(Color.CYAN);
 		setScanColor(Color.BLUE);
 		setAdjustGunForRobotTurn(true);
-		setAdjustRadarForGunTurn(true); // Cañon independiente del giro
+		setAdjustRadarForGunTurn(true); // 
 		
 		do {
 	        turnRadarRightRadians(Double.POSITIVE_INFINITY);
@@ -30,28 +28,29 @@ public class MocardosioHawking3 extends AdvancedRobot {
 
 //Cuando un enemigo está dentro del radar:
 	public void onScannedRobot(ScannedRobotEvent e) {
-		double RandomEnemyHeading = (e.getHeadingRadians() * Math.random())/10;
+		double RandomOffset =  Math.random()/10;
 		double distance = e.getDistance();
+		setTurnRadarLeftRadians(getRadarTurnRemainingRadians());
 	
-		System.out.println("velocidad :" + e.getVelocity());
-		
 		
 		if(distance < 150 && Math.abs(e.getVelocity()) > 4){
-			RandomEnemyHeading = RandomEnemyHeading/2;
+			RandomOffset = RandomOffset/2;
 			
 		}
 		if(distance < 150 && Math.abs(e.getVelocity()) < 4){
-			RandomEnemyHeading = RandomEnemyHeading/10;
+			RandomOffset = RandomOffset/5;
 			
 		}
+		
+		
 			
-		
-		//movimiento del cañon  hacia la derecha siguiendo al tanque enemigo con un pequeño offset random
 		setTurnGunRightRadians(Utils.normalRelativeAngle(getHeadingRadians() + e.getBearingRadians() - 
-			    getGunHeadingRadians()+RandomEnemyHeading));
+			    getGunHeadingRadians()+RandomOffset));
 		
-		System.out.println(RandomEnemyHeading);
-	    setTurnRadarLeftRadians(getRadarTurnRemainingRadians());
+		
+		
+		
+	    
 	    if (distance < 150){
 	    	setFire(16);
 	    }
@@ -70,18 +69,22 @@ public class MocardosioHawking3 extends AdvancedRobot {
 	    setTurnRightRadians(e.getBearingRadians()+(pi/2) );
 		if (getTime() % 25 == 0) {
 			Mov *= -1;
-			randomness = Math.random();
 			setAhead(999 * Mov);
 		}
 		
 	}
 	    
-	
+	public void onWin(WinEvent e) {
+		setAhead(0);
+		setTurnRightRadians(Double.NEGATIVE_INFINITY);	
+		
+		
+	}
 	}    
 	    
 	
 	
-	
+
 
 	
 
